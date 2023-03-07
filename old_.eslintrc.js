1be93cf72daa6d611746c8810aa077d753f78736
Thiago Oliveira <thiago.oliveira208@gmail.com>
2023-03-06 23:05:37 -0300

    Login

---
+++ old_.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": false,
+        "es6": false,
+        "node": false
+    },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+    }
+};
